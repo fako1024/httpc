fe3926c277fcfdc3fd88f1e652b6b6e217ba9ec5
femaref <femaref@googlemail.com>
2022-01-25 13:01:08 +0100

    actually test if the used certs are valid etc.

--- tls_test.go
+++ tls_test.go
@@ -5,15 +5,18 @@ import (
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"math"
 	"math/big"
+	"net"
 	"net/http"
 	"testing"
+	"time"
 )
 
-func constructTLSKeys() (tls.Certificate, []*x509.Certificate, error) {
-	fail := func(err error) (tls.Certificate, []*x509.Certificate, error) {
-		return tls.Certificate{}, nil, err
+func constructTLSKeys() (tls.Certificate, tls.Certificate, []*x509.Certificate, error) {
+	fail := func(err error) (tls.Certificate, tls.Certificate, []*x509.Certificate, error) {
+		return tls.Certificate{}, tls.Certificate{}, nil, err
 	}
 
 	// generate CA key
@@ -31,6 +34,12 @@ func constructTLSKeys() (tls.Certificate, []*x509.Certificate, error) {
 
 	caTemplate := &x509.Certificate{
 		SerialNumber: caSerial,
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:     x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
+
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		IsCA:                  true,
+		BasicConstraintsValid: true,
 	}
 
 	// generate CA cert
@@ -61,6 +70,10 @@ func constructTLSKeys() (tls.Certificate, []*x509.Certificate, error) {
 
 	leafTemplate := &x509.Certificate{
 		SerialNumber: leafSerial,
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
 	}
 
 	// generate leaf cert, signed by CA
@@ -70,24 +83,141 @@ func constructTLSKeys() (tls.Certificate, []*x509.Certificate, error) {
 		return fail(err)
 	}
 
+	// generate leaf key
+	serverKey, err := rsa.GenerateKey(rand.Reader, 1024)
+
+	if err != nil {
+		return fail(err)
+	}
+
+	serverSerial, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+
+	if err != nil {
+		return fail(err)
+	}
+
+	serverTemplate := &x509.Certificate{
+		SerialNumber: serverSerial,
+		NotAfter:     time.Now().Add(365 * 24 * time.Hour),
+
+		KeyUsage: x509.KeyUsageDigitalSignature,
+
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		IPAddresses: []net.IP{net.IPv4(127, 0, 0, 1)},
+	}
+
+	// generate leaf cert, signed by CA
+	serverCert, err := x509.CreateCertificate(rand.Reader, serverTemplate, caCertParsed, &serverKey.PublicKey, caKey)
+
+	if err != nil {
+		return fail(err)
+	}
+
 	tlsCert := tls.Certificate{
 		Certificate: [][]byte{leafCert},
 		PrivateKey:  leafKey,
 	}
+	serverTlsCert := tls.Certificate{
+		Certificate: [][]byte{serverCert},
+		PrivateKey:  serverKey,
+	}
 
-	return tlsCert, []*x509.Certificate{caCertParsed}, nil
+	return tlsCert, serverTlsCert, []*x509.Certificate{caCertParsed}, nil
+}
+
+func configureTLSServer(serverCertWithKey tls.Certificate, caChain []*x509.Certificate) (net.Listener, chan error, error) {
+	caCertPool, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for _, cert := range caChain {
+		caCertPool.AddCert(cert)
+
+	}
+
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCertWithKey},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    caCertPool,
+	}
+
+	ln, err := tls.Listen("tcp", "127.0.0.1:10002", config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	c := make(chan error, 1)
+
+	go func() {
+		for {
+			conn, err := ln.Accept()
+
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
+					return
+				}
+				c <- err
+				continue
+			}
+
+			resp := &http.Response{
+				StatusCode: http.StatusOK,
+			}
+			err = resp.Write(conn)
+			if err != nil {
+				if errors.Is(err, net.ErrClosed) {
+					return
+				}
+				c <- err
+				continue
+			}
+			c <- nil
+		}
+	}()
+
+	return ln, c, nil
 }
 
 func TestCertificateInstance(t *testing.T) {
 	r := New(http.MethodGet, "/")
 
-	clientCertKey, caBytes, err := constructTLSKeys()
+	clientCertKey, serverTlsCert, caChain, err := constructTLSKeys()
 
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_, err = r.ClientCertificatesFromInstance(clientCertKey, caBytes)
+	_, err = r.ClientCertificatesFromInstance(clientCertKey, caChain)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	ln, respChan, err := configureTLSServer(serverTlsCert, caChain)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer ln.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Define request disabling certificate validation
+	req, err := New(http.MethodGet, "https://127.0.0.1:10002/").ClientCertificatesFromInstance(clientCertKey, caChain)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = req.Run()
+	// Execute the request
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = <-respChan
 
 	if err != nil {
 		t.Error(err)
@@ -97,7 +227,7 @@ func TestCertificateInstance(t *testing.T) {
 func TestCertificateInstanceNoPrivateKey(t *testing.T) {
 	r := New(http.MethodGet, "/")
 
-	clientCertKey, caBytes, err := constructTLSKeys()
+	clientCertKey, _, caBytes, err := constructTLSKeys()
 
 	if err != nil {
 		t.Fatal(err)
