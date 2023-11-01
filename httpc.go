f030c70958b7d80a729241826b4b744f36831109
Lennart Elsen <lelsen@open-systems.com>
2023-11-01 15:18:07 -0400

    More debugging

--- httpc.go
+++ httpc.go
@@ -335,7 +335,6 @@ func (r *Request) setBody(req *http.Request) (err error) {
 			dr := newDelayedReader(bytes.NewBuffer(bodyBytes), r.delay)
 
 			req.GetBody = func() (io.ReadCloser, error) {
-				fmt.Println("buf: called GetBody")
 				snapshot := *dr
 				return io.NopCloser(&snapshot), nil
 			}
@@ -347,7 +346,7 @@ func (r *Request) setBody(req *http.Request) (err error) {
 
 		req.GetBody = func() (io.ReadCloser, error) {
 			snapshot := *buf
-			fmt.Println("buf =", snapshot.String(), "; length =", snapshot.Len())
+			fmt.Println(time.Now(), "buf =", buf.String(), "; snapshot =", snapshot.String(), "; length =", snapshot.Len())
 			return io.NopCloser(&snapshot), nil
 		}
 		req.Body = io.NopCloser(buf)
